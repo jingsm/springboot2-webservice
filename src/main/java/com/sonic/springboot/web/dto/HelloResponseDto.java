6808bf9233bd2a29bca902ed36ed70c5c2511b6a
Frank Sim <jingsm@gmail.com>
2020-05-28 00:30:10 +0900

    Class 추가

---
+++ src/main/java/com/sonic/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.sonic.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
