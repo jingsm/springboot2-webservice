f4b12e03231ae1782490e1e5b0c1c740de7896f6
Frank Sim <jingsm@gmail.com>
2020-05-29 23:58:13 +0900

    mustache 추가

--- src/main/java/com/sonic/springboot/Application.java
+++ src/main/java/com/sonic/springboot/Application.java
@@ -2,9 +2,11 @@ package com.sonic.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static org.springframework.boot.SpringApplication.*;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
